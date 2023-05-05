94aec8f72d348432bedf56d82429c92b3aa733f1
ogunnaike folawe matthew <fivestonestech@gmail.com>
2023-05-05 11:31:11 +0100

    Search Filter
    I added search filter to the search box
    Though the code is not currently working but I wanted to wait then check again.

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
-// /SIDEBAR////
+////SIDEBAR////
 const menuItems = document.querySelectorAll('.menu-item');
 
 ///////////////////////  MESSAGE////////////////
-const messagesNotification = document.querySelector
-{'#message-notification'};
+const messagesNotification = document.querySelector('#messages-notification');
 const messages = document.querySelector('.messages');
+const message = messages.querySelectorAll('.message');
+const messageSearch = document.querySelector('#message-search');
+
+
+///////////////////sidebar//////////////////
 //
 // remove active class from all menu items
 const changeActiveItem = () =>{
@@ -28,8 +32,35 @@ menuItems.forEach(item =>{
         }
     })
 })
+  
+////============MESSAGES ===========/////
+//////////SEARCH CHATS////////
+const searchMessage = () => {
+    const val = messageSearch.value.toLowerCase();
+    message.forEach(chat => {
+        let name = chat.querySelectorAll('h5').textContent.toLowerCase
+        ();
+        if(name.indexOf(val) != -1){
+            chat.style.display = 'flex';
+        } else {
+            chat.style.display = 'none';
+        }
+    })
+}
 
-//============MESSAGES ===========/////
+
+
+
+////////////////SEARCH CHAT/////
+messageSearch.addEventListener('keyup', searchMessage);
+
+
+///////hightlight messages card when messages menu item is clicked/////
 messagesNotification.addEventListener('click', () => {
-    messages.style.boxShadow = '0 0 1rem var(--color-primary)';+++++++++++++++ cachesaccccccccccccccccccccccs   ccccccccccccccccccc
+    messages.style.boxShadow = '0 0 1rem var(--color-primary)';
+   messagesNotification.querySelector('.notification-count').style.display 
+   = 'none';
+   setTimeout(() => {
+        messages.style.boxShadow = 'none'
+    }, 2000);
 })
